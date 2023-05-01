7fd3d4f691244b119a154f0577d92156e380660f
Vasiliy Oskolkov <vasya35@live.ru>
2023-05-01 18:29:25 +0300

    Copy js script to clipboard is added

--- TestRunXMLParserTool/Models/JSTestRailSelectorScriptGeneratorModel.cs
+++ TestRunXMLParserTool/Models/JSTestRailSelectorScriptGeneratorModel.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Win32;
 using System.Collections.ObjectModel;
 using System.IO;
+using System.Windows;
 
 namespace TestRunXMLParserTool.Models
 {
@@ -25,30 +26,39 @@ namespace TestRunXMLParserTool.Models
 			{
 				filename = saveFileDialog.FileName;
 			}
-			using TextWriter writer = TextWriter.Synchronized(new StreamWriter(filename));
-			string testCasesList = "";
-			int cnt = 0;
-			foreach (var testCase in selectedTestCases)
-			{
-				if (!testCase.IsSelected) continue;
-				testCasesList += $"'{testCase.TestRailNumber}', ";
-				cnt++;
-			}
-			if (testCasesList.Length > 2)
-			{
-				testCasesList = testCasesList.Remove(testCasesList.Length - 2, 2);
-			}
+			using (TextWriter writer = TextWriter.Synchronized(new StreamWriter(filename))){
+				string testCasesList = "";
+				int cnt = 0;
+				foreach (var testCase in selectedTestCases)
+				{
+					if (!testCase.IsSelected) continue;
+					testCasesList += $"'{testCase.TestRailNumber}', ";
+					cnt++;
+				}
+				if (testCasesList.Length > 2)
+				{
+					testCasesList = testCasesList.Remove(testCasesList.Length - 2, 2);
+				}
 
-			writer.WriteLine($"const testCasesArray = [{testCasesList}];");
-			writer.WriteLine("var xPathRes;");
-			writer.WriteLine("testCasesArray.forEach(element => {");
-			writer.WriteLine("xPathRes = document.evaluate (\"//a[text()='\"+element+\"']/../../td[@class='checkbox']/input\", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);");
-			writer.WriteLine("xPathRes.singleNodeValue.click();");
-			writer.WriteLine("});");
-			writer.WriteLine("var xpath = \"//tr[contains(@class, 'oddSelected') or contains(@class, 'evenSelected')]\"");
-			writer.WriteLine("const result = document.evaluate(xpath, document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);");
-			writer.WriteLine($"console.log('TestCases are selected: ' + result.snapshotLength + ' from {cnt} expected');");
+				writer.WriteLine($"const testCasesArray = [{testCasesList}];");
+				writer.WriteLine("var xPathRes;");
+				writer.WriteLine("testCasesArray.forEach(element => {");
+				writer.WriteLine("xPathRes = document.evaluate (\"//a[text()='\"+element+\"']/../../td[@class='checkbox']/input\", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);");
+				writer.WriteLine("xPathRes.singleNodeValue.click();");
+				writer.WriteLine("});");
+				writer.WriteLine("var xpath = \"//tr[contains(@class, 'oddSelected') or contains(@class, 'evenSelected')]\"");
+				writer.WriteLine("const result = document.evaluate(xpath, document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);");
+				writer.WriteLine($"console.log('TestCases are selected: ' + result.snapshotLength + ' from {cnt} expected');");
+			}			
 
+			CopyToClipboard(filename);
+		}
+
+		private static void CopyToClipboard(string path)
+		{
+			TextReader reader = new StreamReader(path);
+			var text = reader.ReadToEnd();
+			Clipboard.SetText(text);
 		}
 	}
 }
