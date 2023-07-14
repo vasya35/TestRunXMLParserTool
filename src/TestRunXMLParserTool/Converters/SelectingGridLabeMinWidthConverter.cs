47fbc2b792ff8d01e0152bb285904a7b1790a47d
Vasiliy Oskolkov <vasya35@live.ru>
2023-07-14 08:37:29 +0300

    Set minWidth for selecting grids

---
+++ src/TestRunXMLParserTool/Converters/SelectingGridLabeMinWidthConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace TestRunXMLParserTool.Converters
+{
+	[ValueConversion(typeof(int), typeof(int))]
+	public class SelectingGridLabeMinWidthConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			return (value.ToString().Length * 2 + 1) * 10;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			return null;
+		}
+	}
+}
