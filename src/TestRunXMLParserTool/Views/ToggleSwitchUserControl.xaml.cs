3e5ce9cbbbfd4de7a5806bc20b0d83fc4de4bf17
Vasiliy Oskolkov <vasya35@live.ru>
2023-07-18 21:30:15 +0300

    Add ToggleSwitch UserControl

---
+++ src/TestRunXMLParserTool/Views/ToggleSwitchUserControl.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace TestRunXMLParserTool.Views
+{
+	/// <summary>
+	/// Interaction logic for ToggleSwitchUserControl.xaml
+	/// </summary>
+	public partial class ToggleSwitchUserControl : UserControl
+	{
+		#region fileds
+		private static readonly NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
+		#endregion
+
+		public ToggleSwitchUserControl()
+		{
+			InitializeComponent();
+		}
+
+		private void SwitchByMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+		{
+			try
+			{
+				DataContext = !(bool)DataContext;
+			}
+			catch (System.Exception error)
+			{
+				Logger.Warn($"DataContext for ToggleSwitchUserControl wasn't set: {error}");
+			}
+		}
+	}
+}
